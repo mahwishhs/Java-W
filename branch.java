cc544c1485c255d68dd1775fc71bf0228264b27f
mahwishhs <mahwixh.seemi@gmail.com>
2023-07-07 12:07:17 +0500

    new branch

--- branch.java
+++ branch.java
@@ -1,5 +1,5 @@
 public class branch {
     public static void main(String[] args) {
-        System.out.println("This is new branch!");
+        System.out.println("This is new branch! hehe");
     }
 }
